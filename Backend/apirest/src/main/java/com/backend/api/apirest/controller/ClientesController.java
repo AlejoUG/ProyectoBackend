4f1b6105181dd984ac6442a9f160a41050f04981
Alejandro Utria Garcia <alejandroutriag@gmail.com>
2022-01-03 15:42:46 -0500

    Se hizo un front pero no se termino, de agregar usuario

--- Backend/apirest/src/main/java/com/backend/api/apirest/controller/ClientesController.java
+++ Backend/apirest/src/main/java/com/backend/api/apirest/controller/ClientesController.java
@@ -10,35 +10,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.api.apirest.model.Clientes;
 
 @RestController
+@RequestMapping("api/clientes")
+@CrossOrigin(origins = "http://localhost:4200", maxAge=3600)
 public class ClientesController {
 	
 	@Autowired
 	private ClientesService clientesService;
 	
-	@PostMapping("api/clientes")
+	@PostMapping
 	public Clientes guardarClientes(@RequestBody Clientes clientes) {
 		System.out.println(clientes);
 		clientesService.guardar(clientes);
 		return clientes;
 	}
 	
-	@GetMapping("api/clientes/{numCuenta}")
+	@GetMapping("/clientes/{numCuenta}")
 	public Clientes obtenerCliente(@PathVariable("numCuenta") Integer numCuenta) {
 		return clientesService.obtenerCliente(numCuenta);
 	}
 	
-	@PutMapping("api/clientes/{numCuenta}")
+	@PutMapping("/clientes/{numCuenta}")
 	public ResponseEntity<?> actualizarCliente(@RequestBody Clientes clientesDetalles, @PathVariable("numCuenta") Integer numCuenta){
 		Optional<Clientes> clientes = clientesService.findByNumCuenta(numCuenta);
 		
@@ -56,12 +60,12 @@ public class ClientesController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(clientesService.save(clientes.get()));
 	}
 			
-	@DeleteMapping("api/clientes/{numCuenta}")
+	@DeleteMapping("/clientes/{numCuenta}")
 	public void eliminar(@PathVariable("numCuenta") Integer numCuenta) {
 		clientesService.eliminar(numCuenta);
 	}
 	
-	@GetMapping("api/clientes/{numCuenta}/estado")
+	@GetMapping("/clientes/{numCuenta}/estado")
 	public List<Clientes> obtenerClientesActivos(@PathVariable("numCuenta") Integer numCuenta, 
 			@PathParam("estado") String estado) {
 		return clientesService.buscarClientesActivos(numCuenta, estado);
